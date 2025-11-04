0b5280ad1809b7b9c900861f7f2b868ad18f2682
MaxNoragami <maxcraftmd@gmail.com>
2025-11-04 14:51:53 +0200

    chore: add the client code to be served

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors();
+
+
 var app = builder.Build();
 
+
 app.MapGet("/", () => "Hello World!");
+
+
+app.UseCors(policy => policy
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+app.UseStaticFiles();
+
 
 app.Run();
