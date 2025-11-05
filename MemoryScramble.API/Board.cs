818799ecabe54fb32b53c2f6861132f97ab7d7c3
MaxNoragami <maxcraftmd@gmail.com>
2025-11-05 10:41:45 +0200

    chore: add starting point for the Board implementation

---
+++ MemoryScramble.API/Board.cs
@@ -0,0 +1,52 @@
+﻿namespace PR_lab3_MemoryScramble;
+
+public class Board
+{
+    private class Cell(string? card)
+    {
+        public string? Card { get; set; } = card;
+        public bool IsUp { get; set; }
+    }
+
+    private readonly Cell[,] _grid;
+    private readonly Dictionary<(int Row, int Column), string> _controlledBy;
+
+
+    public int Rows { get; init; }
+    public int Columns { get; init; }
+
+
+    private Board(int rows, int columns, Cell[,] grid) 
+    {
+        Rows = rows;
+        Columns = columns;
+        _grid = grid;
+        _controlledBy = new();
+        CheckRep();
+    }
+
+    public static async Task<Board> ParseFromFile(string fileName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Look(string playerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Flip(string playerId, int row, int column)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void CheckRep()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ MemoryScramble.API/Commands.cs
@@ -0,0 +1,10 @@
+﻿namespace PR_lab3_MemoryScramble;
+
+public static class Commands
+{
+    public static Task<string> Look(Board board, string playerId)
+        => Task.FromResult(board.Look(playerId));
+
+    public static Task<string> Flip(Board board, string playerId, int row, int column)
+        => Task.FromResult(board.Flip(playerId, row, column));
+}
